3b08862f75c2acc06e018deb6bca824939a7af0a
Ismail Dondu <ismaildondu7@gmail.com>
2022-11-21 13:42:49 +0300

    Add modern close icon(svg)

--- DNDAlert.js
+++ DNDAlert.js
@@ -252,7 +252,7 @@ class DNDAlert extends ALERT_CONTEXT {
   createTopRightCloseButton() {
     let close_button = document.createElement("button");
     close_button.classList.add(this.CLASS_LIST[this.THEME].close_button);
-    close_button.innerHTML = "X";
+    close_button.innerHTML = this.SVG_LIST.close;
     if (this.THEME === this.THEME_ENUM.DARK) {
       close_button.style.color = "#000";
     }
@@ -496,12 +496,14 @@ class DNDAlert extends ALERT_CONTEXT {
         error: "#e74c3c",
         warning: "#fab005",
         info: "#3498db",
+        close: "#212529",
       },
       dark: {
         success: "#40c057",
         error: "#ff6b6b",
         warning: "#fcc419",
         info: "#3bc9db",
+        close: "#fff",
       },
     };
     this.SVG_LIST = {
@@ -530,6 +532,12 @@ class DNDAlert extends ALERT_CONTEXT {
       <circle cx="12" cy="12" r="10"></circle>
       <line x1="12" y1="16" x2="12" y2="12"></line>
       <line x1="12" y1="8" x2="12.01" y2="8"></line>
+      </svg>
+      `,
+      close: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="${this.SVG_COLOR_LIST[theme].close}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
       </svg>
       `,
     };
